372a38a48a7cbb554499c8e5cfdefe19e25d59c2
Daniele Cassia <danielecassia9@gmail.com>
2023-09-12 21:59:35 -0300

    fix: limpa o app e adiciona contexto na main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { UserDataProvider } from './contexts/UserDataContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UserDataProvider>
+      <App />
+    </UserDataProvider>
   </React.StrictMode>,
 )
