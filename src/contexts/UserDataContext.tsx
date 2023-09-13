612a995909f0ba6ac63a0eace9d7d5d6040c7bcd
Daniele Cassia <danielecassia9@gmail.com>
2023-09-12 21:58:26 -0300

    feat: cria contexto

---
+++ src/contexts/UserDataContext.tsx
@@ -0,0 +1,75 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { UserData } from '../types/UserData';
+
+
+//Tipando as Props do contexto
+interface PropsUserContext {
+  formList: UserData[];
+  addDataToFormList: (newUserData: UserData) => void
+}
+
+// criando contexto
+const UserDataContext = createContext<PropsUserContext | undefined>(undefined);
+
+type Props = {
+  children: React.ReactNode
+}
+
+const UserDataProvider: React.FC<Props> = ({ children }) => {
+  const [formList, setFormList] = useState([] as UserData[]);
+
+  function addDataToFormList(newUserData: UserData) {
+    setFormList([...formList, newUserData])
+  }
+
+  // function deleteDataToFormList(){
+
+  // }
+
+  // carrega a lista de formulários preenchidos
+  useEffect(() => {
+    const oldFilledForms = localStorage.getItem('filledForms')
+    if (!oldFilledForms) {
+      console.log("nao")
+      return
+    }
+    const filledForms = JSON.parse(oldFilledForms);
+    if (filledForms) {
+      console.log("deu")
+      setFormList(filledForms);
+    }
+    else {
+      console.log("deu nao")
+    }
+  }, [])
+
+  // guarda a lista de formulários preenchidos
+  useEffect(() => {
+    if (formList.length == 0)
+      return
+    localStorage.setItem('filledForms', JSON.stringify(formList))
+    console.log("salvou")
+  }, [formList])
+
+  return (
+    <UserDataContext.Provider
+      value={{
+        addDataToFormList,
+        formList
+      }}
+    >
+      {children}
+    </UserDataContext.Provider>
+  );
+};
+
+function useUserData() {
+  const context = useContext(UserDataContext);
+  if (context === undefined)
+    throw new Error("useUserData must be within UserDataProvider")
+  return context;
+}
+export { UserDataProvider, useUserData };
+
+
+
