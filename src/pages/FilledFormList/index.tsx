77fe613ff67a81fa7550f26408b0b8d5108c373f
Daniele Cassia <danielecassia9@gmail.com>
2023-09-12 23:15:13 -0300

    fix: cria lista baseado no objeto

--- src/pages/FilledFormList/index.tsx
+++ src/pages/FilledFormList/index.tsx
@@ -1,7 +1,26 @@
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { useUserData } from "../../contexts/UserDataContext";
+import { Link } from 'react-router-dom';
 
 export function FilledFormList() {
-
+  const { formList } = useUserData();
+  console.log(formList);
   return (
-    <div>list</div>
-  )
+    <div>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        aria-label="contacts"
+      >
+        {formList.map((listItem) =>
+          <ListItem disablePadding>
+            <ListItemButton
+              component={Link}
+              to={`../${listItem.name}/details`}>
+              <ListItemText primary={listItem.name} />
+            </ListItemButton>
+          </ListItem>
+        )}
+      </List>
+    </div>
+  );
 }
